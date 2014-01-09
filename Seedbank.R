72f05f4bd144cfcd3232d887b39117851f0d0418
unknown <elm26@FH-NS-HYB2.human.cornell.edu>
2014-01-09 16:33:51 -0500

    work on Jan 9

--- Seedbank.R
+++ Seedbank.R
@@ -103,6 +103,7 @@ legend(1,0.95*yplotmax, levels(thisCensus$TranDir), col=dircols, lwd=2, lty=1, b
 
 
 # Seedbank Summaries --------------
+
 library(doBy)
 stderr <- function(x) sd(x)/sqrt(length(x))
 
@@ -159,9 +160,9 @@ arrows(sub.Seedbank$InvDens.mean, sub.Census$InvDens.mean,  sub.Seedbank$InvDens
 #Try plotting all on same graph---------
 #Invasive Density-------
 #Do north and South separately
-DESERT="Mojave"
-SeedYEAR="2011"
-CensusYEAR="2011"
+DESERT="Sonoran"
+SeedYEAR="2010"
+CensusYEAR="2012"
 TRANDIR="S"
 
 sub.Census=subset(Census.summary,(Desert==DESERT)&(Year==CensusYEAR)&(TranDir==TRANDIR))
@@ -191,7 +192,45 @@ for(FIRETRT in FireTrtmnts){
             try(arrows(sub.Seed$InvDens.mean, sub.Cen$InvDens.mean,  sub.Seed$InvDens.mean, sub.Cen$InvDens.mean+sub.Cen$InvDens.stderr, angle=90, length=0.05, col=thiscol, pch=thispch, lty=thislty))
             try(arrows(sub.Seed$InvDens.mean, sub.Cen$InvDens.mean,  sub.Seed$InvDens.mean, sub.Cen$InvDens.mean-sub.Cen$InvDens.stderr, angle=90, length=0.05, col=thiscol, pch=thispch, lty=thislty))
             #text(sub.Seed$InvDens.mean, sub.Cen$InvDens.mean, c(1,2,3,4), pos=2)
+        }
+    }
 }
+
+#Native Density-------
+#Do North and South separately
+DESERT="Sonoran"
+SeedYEAR="2012"
+CensusYEAR="2012"
+TRANDIR="S"
+
+sub.Census=subset(Census.summary,(Desert==DESERT)&(Year==CensusYEAR)&(TranDir==TRANDIR))
+sub.Seedbank=subset(Seedbank.summary,(Desert==DESERT)&(Year==SeedYEAR)&(TranDir==TRANDIR))
+xrange=range(c(sub.Seedbank$NatDens.mean+sub.Seedbank$NatDens.stderr, sub.Seedbank$NatDens.mean-sub.Seedbank$NatDens.stderr))
+yrange=range(c(sub.Census$NatDens.mean+sub.Census$NatDens.stderr,sub.Census$NatDens.mean+sub.Census$NatDens.stderr))
+windows(6,6)
+plot(0,0, xlab="Seed Bank Density", ylab="Census Density", xlim=xrange, ylim=yrange, pch=NA, 
+     main=c(paste(DESERT, " ", SeedYEAR, " Seedbank     ", CensusYEAR , " Census "), paste( TRANDIR, " NatDens") ))
+
+#expand.grid(Fire=c("B","UB"), Rain=c("A", "E"), Turb=c("Y","N"))
+
+FireTrtmnts=c("B", "UB"); RainTrtmnts=c("A", "E"); TurbTrtmnts=c("Y","N")
+for(FIRETRT in FireTrtmnts){
+    for(RAINTRT in RainTrtmnts){
+        for(TURBTRT in TurbTrtmnts){
+            sub.Cen=sub.Seed=NULL
+            if (FIRETRT=="B") thispch=16 else thispch=17
+            if (RAINTRT=="A") thiscol="brown" else thiscol="blue"
+            if (TURBTRT=="Y") thislty=1 else thislty=3
+            sub.Cen=subset(sub.Census,(FireTrt==FIRETRT)&(RainTrt==RAINTRT)&(TurbTrt==TURBTRT))
+            print(sub.Cen)
+            sub.Seed=sub.Seedbank
+            try(points(sub.Seed$NatDens.mean, sub.Cen$NatDens.mean, col=thiscol, pch=thispch))
+            try(arrows(sub.Seed$NatDens.mean, sub.Cen$NatDens.mean,  sub.Seed$NatDens.mean+sub.Seed$NatDens.stderr, sub.Cen$NatDens.mean, angle=90, length=0.05, col=thiscol, pch=thispch, lty=thislty))
+            try(arrows(sub.Seed$NatDens.mean, sub.Cen$NatDens.mean,  sub.Seed$NatDens.mean-sub.Seed$NatDens.stderr, sub.Cen$NatDens.mean, angle=90, length=0.05, col=thiscol, pch=thispch, lty=thislty))
+            try(arrows(sub.Seed$NatDens.mean, sub.Cen$NatDens.mean,  sub.Seed$NatDens.mean, sub.Cen$NatDens.mean+sub.Cen$NatDens.stderr, angle=90, length=0.05, col=thiscol, pch=thispch, lty=thislty))
+            try(arrows(sub.Seed$NatDens.mean, sub.Cen$NatDens.mean,  sub.Seed$NatDens.mean, sub.Cen$NatDens.mean-sub.Cen$NatDens.stderr, angle=90, length=0.05, col=thiscol, pch=thispch, lty=thislty))
+            #text(sub.Seed$NatDens.mean, sub.Cen$NatDens.mean, c(1,2,3,4), pos=2)
+        }
     }
 }
 
@@ -199,12 +238,27 @@ for(FIRETRT in FireTrtmnts){
 
 
 
-#Try fitting mixed effects model to Census data, and for a given trt combo,
+#Try fitting mixed effects model to Census data (and Seedbank Data?), and for a given trt combo,
 # Come up with LSMEANS, for each ones- This will allow for using more data.
 # Then compare the seedbank value for these means. 
-# Treat seedbank as a covariate. Is covariate (seedbank) significant. 
+# Treat seedbank as a covariate. Is covariate (seedbank) significant? 
 # 
+Seedbank$Year=as.factor(Seedbank$Year)
+
+Seedbank.glme=glmer( NatDens~Year+MH+Desert+TranDir+ 
+                    Year:MH + Year:Desert + Year:TranDir + # MH:Desert + Desert:TranDir+  MH:TranDir +
+                        #Year:Desert:TranDir + Year:MH:TranDir + Year:MH:Desert+ MH:Desert:TranDir + 
+                    (1|ShrubID/Plot) , family=poisson, data=Seedbank[Seedbank$GermPush==1234,])
+
+summary(Seedbank.glme)
+
+lsmeans(Seedbank.mod, pairwise~  Year:MH )
+lsmeans(Seedbank.mod, pairwise~  Year:Desert )
+lsmeans(Seedbank.mod, pairwise~   Year:TranDir)
 
 
+Seedbank.mod=glmer( NatDens~Year*MH+Desert+ (1|ShrubID/Plot) , family=poisson, data=Seedbank[Seedbank$GermPush==1234,])
+summary(Seedbank.mod)
 
-
+lsmeans(Seedbank.mod, pairwise~Year*MH)
+lsmeans(Seedbank.mod, pairwise~Desert)
